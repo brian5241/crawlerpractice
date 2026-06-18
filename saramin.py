76745b091df211adfebb1e4979e8418599e86e05
zkskcyqk <haqemah@gmail.com>
2026-06-18 14:34:07 +0900

    크롤링 수업완성

---
+++ saramin.py
@@ -0,0 +1,121 @@
+from bs4 import BeautifulSoup #정적인 크롤링
+import requests #인터넷 내용을 크롤링
+import pandas as pd # df정리
+import re #정규표현식(내용을 예쁘게 다듬을 때 사용)
+from html import unescape #html형식에서 깨진 글자를 복원
+
+def clean_text(text):
+    #text를 넣어서 clean_text를 실행했는데, text가 None이라면?
+    if not text:
+        return ''
+    
+    #text가 있다면(빈 값이 아니라면) 정제
+    text = unescape(text)
+
+    #re(정규표현식) : 텍스트를 특정한 패턴으로 찾아서, 변형
+    #re.sub(패턴, 바꿀 문자, 문장) -> 문장에서 '패턴'을 찾아서 '바꿀문자'로 변형
+    #a-zA-Z : 영문자 전체, ㄱ-ㅎ가-힣
+    # \s : (공백) ////  \s+ : (한 칸 이상의 모든 공백)
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
+
+def crawling_data(search_word):
+    #1. '주소'를 정함(가져올 정보가 있는 인터넷 주소)
+    #2. requests.get(주소) => 그 '주소'에 있는 html을 받아옴
+    # html : 인터넷에서 정보를 표현하는 파일 형식
+    #3. bs4 -> 받아온 html을 파싱(parsing) , 필요정부 추출
+    #4. bs4.select(구분자) <div>, </div> 등
+    #5. bs4.select_one(구분자)
+    #6. 얻어낸 정보 -> re(정규표현식)으로 '정제' or pandas로 정리해서 저장
+
+
+    #검색창과 결과 페이지가 같이 나오는걸 찾아서 뒤부터 소거해가면됨.
+    base_url = 'https://www.saramin.co.kr/zf_user/search'
+    #헤더 : 브라우저가 서버에 무언가 요청할때 '나 이런 브라우저에요'라는 내용
+    headers = {
+        #요청하는 브라우저의 종류가 Mozilla/AppleWebKit/Chrome/Safari 중에 있다는 것
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/126.0.0.0 Safari/537.36"
+        ),
+        #요청하는 언어의 종류
+        "Accept-Language": "ko-KR,ko;q=0.9,en;q=0.8",
+    }
+
+
+    #리퀘스트를 보냄
+    #headers : 웹페이지에 정보를 요청하는 브라우저에 대한 내용
+    #params : 웹페이지에 이런 정보를 요청합니다 하는 '검색어, 검색조건'
+    #timeout : 웹페이지로부터 회신이 올 때까지 기다리는 시간 최대치
+    response = requests.get(base_url, headers = headers, 
+                            params = {'searchword' : search_word},
+                            timeout = 10)
+
+    #만약 response가 <200> 이라면 => 정상 /400이나 500으로 시작하면 에러
+    #400에러 : 서버에러 #500에러 : 클라이언트 에러
+    #bs4에게 html을 파싱하도록 객체 생성 BeautifulSoup(html, 파서)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # print(response.text)
+
+    # 검색 결과 선별
+    rows = []
+
+    #soup.select(구분자) : '구분자'가 들어가는 모든 내용을 select
+    #find_all과의 차이점 : find가 옛날 방식. 전통적인 선택방식 / select가 현대적
+    #soup.find_all('div', class_ = 'item_recruit')와 동일
+    for item in soup.select('div.item_recruit'):
+        #1. 회사명
+        #select_one(구분자) : 구분자 이름을 갖는 딱 하나만 가져오란 뜻
+        corp_name = item.select_one('div.area_corp')
+
+        #2. 채용 정보
+        job_area = item.select_one('div.area_job')
+
+        #3. 공고 제목
+        #<div>로 시작하지 않으므로 .job tit / div가 아니여도 괜찮다
+        #job_tit를 찾아서 one 한개만 가져오라는 뜻
+        job_title = job_area.select_one('.job_tit')
+
+        #4. 조건
+        conditions = job_area.select_one('.job_condition')
+        location = '' #초기화(깨끗한 값으로 먼저 덮어씌우고 시작)
+        condition1 = '' #예외처리를 한 것
+
+        if conditions:
+            span = conditions.select('span')
+            #조건이 1개 이상 있다면
+            if len(span) > 0:
+                #첫번째 조건은 무조건위치이다. (공고 목록을 보고 알아낼 수 있음.)
+                location = span[0].get_text(strip = True)
+            
+            if len(span) > 1:
+                condition1 = span[1].get_text(strip = True)
+
+        #5. 직무 분야
+        job_sector = job_area.select_one('.job_sector')
+        job_sector = (job_sector.get_text(strip = True) if job_sector else "")
+
+        #내가 모은 정보를 '정제' 함
+        job_title = clean_text(job_title)
+        location = clean_text(location)
+        condition1 = clean_text(condition1)
+        job_sector = clean_text(job_sector)
+        corp_name = clean_text(corp_name)
+
+        #rows 리스트, 리스트 안에 딕셔너리 -> 판다스로 만들기위해
+        rows.append({
+            '공고 이름' : job_title,
+            '회사 위치' :  location, 
+            '조건 1' : condition1, 
+            '조건 2' : job_sector, 
+            '회사 이름' : corp_name
+        })
+
+    #최종적으로 얻어진 rows를 pd.DataFrame으로 감싸서 df로 맏늘어줌
+    df = pd.DataFrame(rows)
+    print(df)
+
+#진입점(이 파이썬 파일의 '실제 실행되는 부분')
+if __name__ == '__main__':
+    crawling_data('인공지능')
